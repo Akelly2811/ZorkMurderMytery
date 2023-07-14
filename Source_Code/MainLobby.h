f665fe6adead0d2906cd8f1117d804adfe9eb2f2
Andrew Kelly <andrewkellyap@gmail.com>
2023-07-14 13:46:25 +0100

    Uploading Game to GIT

---
+++ Source_Code/MainLobby.h
@@ -0,0 +1,29 @@
+#ifndef MAINLOBBY_H
+#define MAINLOBBY_H
+#include "Room.h"
+#include "animateobject.h"
+
+class MainLobby : public Room
+{
+    private:
+        QString description;
+        map<string, Room*> exits;
+        QString exitString();
+        vector <Character> CharactersInRoom;
+        friend class mainwindow;
+
+    public:
+        MainLobby(QString description, Item *newItem, Item *newItem2);
+        ~MainLobby();
+        QString shortDescription() override;
+        void addCharacter(Character *character);
+        void addAnimateObject(AnimateObject *animateObject);
+        Character getCharacter(QString name);
+        QString displayCharacter();
+        int isCharacterInRoom(QString inQString);
+        vector<Character> getCharacterList();
+        int numberOfCharacters();
+
+};
+
+#endif // MAINLOBBY_H
